9e26415fbd2e7012ffcc5ea9f542133a73d7659b
keef <qwertykeith@yahoo.com>
2017-02-11 14:28:11 +1100

    new sounds and more dots

--- src/welcome.ts
+++ src/welcome.ts
@@ -6,7 +6,7 @@ import { KLoop } from './models/kloop'
 
 export class Welcome {
 
-  dots: Dot[];
+  dots: Dot[] = [];
   somenum: number = 0;
   player: KSamplePlayer;
 
@@ -17,7 +17,23 @@ export class Welcome {
     "https://s3-ap-southeast-2.amazonaws.com/ksounds/MoM Tap Kick 55-10.wav",
     "https://s3-ap-southeast-2.amazonaws.com/ksounds/MoM Klick 08 57-08.wav",
     "https://s3-ap-southeast-2.amazonaws.com/ksounds/MoM Klick 10 57-10.wav",
-    "https://s3-ap-southeast-2.amazonaws.com/ksounds/MoM Klick 06 57-06.wav"
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/MoM Klick 06 57-06.wav",
+
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Clank/MoM+Chunk+02+56-02.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Clank/MoM+Goose+Flab+63-03.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Clank/MoM+Rim+Tick+63-04.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Clank/MoM+Wash+Hit+62-09.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Clank/MoM+Clank+12+53-12.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Snares/MoM+Ugly+Acoustic++08+64-12.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Snares/MoM+Mouth+Snare+63-07.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Snares/MoM+Clang+Snare+62-02.wav",
+    "https://s3-ap-southeast-2.amazonaws.com/ksounds/Kiks/MoM+Klick+Kick+62-06.wav",
+    // "",
+    // "",
+    // "",
+    // "",
+    // "",
+
   ];
 
   constructor() {
@@ -66,19 +82,27 @@ export class Welcome {
 
 
 
-    this.dots = [
-      { id: 1, loop: new KLoop(this.soundUlrs[0], "16n"), pos: { x: 0, y: 0 } },
-      { id: 2, loop: new KLoop(this.soundUlrs[1], "4n"), pos: { x: 20, y: 1 } },
-      { id: 3, loop: new KLoop(this.soundUlrs[2], "8n"), pos: { x: 300, y: 2 } },
-      { id: 4, loop: new KLoop(this.soundUlrs[3], "32n"), pos: { x: 200, y: 10 } },
-      { id: 5, loop: new KLoop(this.soundUlrs[4], "16n"), pos: { x: 150, y: 90 } },
-      { id: 6, loop: new KLoop(this.soundUlrs[5], "8n"), pos: { x: 90, y: 50 } },
-      { id: 7, loop: new KLoop(this.soundUlrs[0], "12n"), pos: { x: 20, y: 10 } },
-    ];
+    // this.dots = [
+    //   { id: 1, loop: new KLoop(this.soundUlrs[0], "16n"), pos: { x: 0, y: 0 } },
+    //   { id: 2, loop: new KLoop(this.soundUlrs[1], "4n"), pos: { x: 20, y: 1 } },
+    //   { id: 3, loop: new KLoop(this.soundUlrs[2], "8n"), pos: { x: 300, y: 2 } },
+    //   { id: 4, loop: new KLoop(this.soundUlrs[3], "32n"), pos: { x: 200, y: 10 } },
+    //   { id: 5, loop: new KLoop(this.soundUlrs[4], "16n"), pos: { x: 150, y: 90 } },
+    //   { id: 6, loop: new KLoop(this.soundUlrs[5], "8n"), pos: { x: 90, y: 50 } },
+    //   { id: 7, loop: new KLoop(this.soundUlrs[0], "12n"), pos: { x: 20, y: 10 } },
+    // ];
 
-    this.dots.forEach(dot => {
-      this.player.addSound(dot.loop);
-    });
+    // this.dots.forEach(dot => {
+    //   this.player.addSound(dot.loop);
+    // });
+
+const beats=['8n','16n','8n','16n','8n','12n','16n','8n','12n','16n','32n'];
+// const beats=['4n','8n','12n','16n','32n'];
+
+    // randomly add stuff
+    for (let i = 0; i < 200; i++) {
+      this.newDot(beats);
+    }
 
   }
 
@@ -92,8 +116,8 @@ export class Welcome {
     const beats = theBeats || ['8n', '16n', '32n'];
     const sound = this.soundUlrs[Math.floor(this.soundUlrs.length * Math.random())]
     const beat = beats[Math.floor(beats.length * Math.random())]
-    const x = Math.floor(300 * Math.random());
-    const y = Math.floor(300 * Math.random());
+    const x = Math.floor(400 * Math.random());
+    const y = Math.floor(400 * Math.random());
 
     var dot = { id: 7, loop: new KLoop(sound, beat), pos: { x: x, y: y } };
     console.log('NEW dot');
